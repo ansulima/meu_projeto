d472748ae0c16e11859fa771f6f5ce25ff768c67
Anderson Freitas <freitaslimaanderson@gmail.com>
2022-12-05 22:37:54 -0300

    criando listas de exercicios

---
+++ Exercicio_1.rb
@@ -0,0 +1,14 @@
+class NomeDaClasse
+  def imprimir_ola(nome)
+    @nome = nome
+    puts "Olá #{@nome}"
+  end
+
+  def nome
+    @nome
+  end
+end
+
+anderson = NomeDaClasse.new
+anderson.imprimir_ola("Anderson")
+anderson.nome
