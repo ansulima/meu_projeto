519e5b38ea2f92207c93f6a633413ae88b5bbbfe
Anderson Freitas <freitaslimaanderson@gmail.com>
2022-12-07 19:29:28 -0300

    percorrer um array

---
+++ Lista_de_Exercicios/Exercicio_1.rb
@@ -0,0 +1,7 @@
+=begin
+
+Problem: Given a sorted array arr[] of n elements, 
+write a function to search a given element x in arr[] and return the index of x in the array.
+Consider array is 0 base index.
+    
+=end
