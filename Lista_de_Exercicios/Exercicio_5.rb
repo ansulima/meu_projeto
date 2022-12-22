835d03892f4eb6474e8b373f6760b0c9f669d8fa
Anderson Freitas <freitaslimaanderson@gmail.com>
2022-12-22 09:22:10 -0300

    Usando bubble sort

--- Lista_de_Exercicios/Exercicio_5.rb
+++ Lista_de_Exercicios/Exercicio_5.rb
@@ -1,2 +1,35 @@
-def MinhaClasse
-end
+class Bubblesort
+    attr_reader :array_sorted
+  
+    def initialize
+      @array_sorted = []
+    end
+  
+    def init(array)
+      bubble_sort(array)
+    end
+  
+    private
+  
+    def bubble_sort(array)
+      return nil if array.empty?
+      n = array.length - 1
+      loop do
+        swapped = false
+  
+        n.times do |i|
+          if array[i] > array[i + 1]
+            array[i], array[i + 1] = array[i + 1], array[i]
+            swapped = true
+          end
+        end
+        break unless swapped
+      end
+      @array_sorted = array
+    end
+  end
+  
+  # test
+  b_s = Bubblesort.new
+  b_s.init([1,4,10,11,2,3,31,32,0])
+  p b_s.array_sorted
