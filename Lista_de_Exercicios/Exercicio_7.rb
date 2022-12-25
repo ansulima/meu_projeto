664f93d0d7fa78eb03e9ad6e6b3780b0376cdc31
Anderson Freitas <freitaslimaanderson@gmail.com>
2022-12-25 03:19:56 -0300

    Calculadora simples

---
+++ Lista_de_Exercicios/Exercicio_7.rb
@@ -0,0 +1,42 @@
+
+def soma
+    x = 1
+    puts "Digite um numero para saber sua tabela:"
+    num = gets.chomp.to_i
+    while x <= 10
+        puts "#{x} + #{num} = #{x + num}"
+        x += 1
+    end
+end
+
+def sub
+    x = 1
+    puts "Digite um numero para saber sua tabela:"
+    num = gets.chomp.to_i
+    while x <= 10
+        puts "#{x} - #{num} = #{x - num}"
+        x += 1
+    end
+end
+
+number = ''
+
+loop do
+    puts "Escolha uma opção:"
+
+    puts "1 - Somar"
+    puts "2 - Subtrair"
+    puts "3 - Dividir"
+    puts "4 - Muliplicar"
+    puts "0 - Sar do programa"
+
+    print "Opção: "
+    option = gets.chomp.to_i
+
+    if option == 1
+        puts "#{soma}"
+    elsif option == 2
+        puts "#{sub}"
+    end
+        
+end
