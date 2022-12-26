b29bf4212ec8f84fbd7bdb23055d25e08dfaef62
Anderson Freitas <freitaslimaanderson@gmail.com>
2022-12-26 19:41:29 -0300

    contando os elementos do array

--- Lista_de_Exercicios/Exercicio_8.rb
+++ Lista_de_Exercicios/Exercicio_8.rb
@@ -1,15 +1,16 @@
 def estado
     x = 1
     puts "Digite o nome de 5 estados"
+   
     while x <= 5
         num_estado = gets.chomp.to_s
-        num_estado = []
+        num_estado.new = []
         x += 1
     end
+    puts "Sua lista tem um total de #{num_estado.count()} elemento"
 end
 
 
 
 
 puts "#{estado}"
-puts "Sua lista tem um total de #{num_estado.count} elementos"
