38a106c14de10e897da293c4fa02f973dd6d9e02
Anderson Freitas <freitaslimaanderson@gmail.com>
2022-12-26 19:27:36 -0300

    Criando arrays com nomes de estados

--- Lista_de_Exercicios/Exercicio_8.rb
+++ Lista_de_Exercicios/Exercicio_8.rb
@@ -1,3 +1,15 @@
-estados = []
-estados.push('Sp', 'Rio Grande do Sul', 'Ceará')
-puts estados
+def estado
+    x = 1
+    puts "Digite o nome de 5 estados"
+    while x <= 5
+        num_estado = gets.chomp.to_s
+        num_estado = []
+        x += 1
+    end
+end
+
+
+
+
+puts "#{estado}"
+puts "Sua lista tem um total de #{num_estado.count} elementos"
