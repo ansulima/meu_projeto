55dfe694a461dda297a221f2ddabaac3146126b9
Anderson Freitas <freitaslimaanderson@gmail.com>
2022-12-27 05:55:30 -0300

    usando .map e .map!

--- Lista_de_Exercicios/Exercicio_8.rb
+++ Lista_de_Exercicios/Exercicio_8.rb
@@ -1,5 +1,5 @@
 # .each
-'''names = ['Joãozinho', 'Manoel', 'Juca']
+=begin names = ['Joãozinho', 'Manoel', 'Juca']
 
 name = 'Anderson Freitas'
 
@@ -8,13 +8,32 @@ names.each do |name|
 end
 
 puts name
-'''
+=end
 
 # usando o .each em hash
 
-aulas = {'Aula 1' => 'Liberada', 'Aula 2' => 'Liberada', 'Aula 3' => 'Liberada', 'Aula 4' => 'Liberada', 'Aula 5' => 'Em breve'}
+=begin aulas = {'Aula 1' => 'Liberada', 'Aula 2' => 'Liberada', 'Aula 3' => 'Liberada', 'Aula 4' => 'Liberada', 'Aula 5' => 'Em breve'}
 
 aulas.each do |key, value|
     puts "#{key} #{value}"
 end
 
+=end
+
+# usando o .map
+
+array =[1, 2, 3, 4]
+
+puts "\n Executando .map multiplicando cada item por 2"
+
+# O Array.map ! altera o array inicial(apagando o inicial)
+# O Array.map cria um novo array
+new_array = array.map! do |a|
+    a + 2
+end
+
+puts "\n Array orginal"
+puts " #{array}"
+
+puts "\n Novo array"
+puts " #{new_array}"
