9d2aad34a9fd409a7be39e7d972f5b8d2e6ade79
Anderson Freitas <freitaslimaanderson@gmail.com>
2022-12-27 05:33:36 -0300

    usando .each em collections

--- Lista_de_Exercicios/Exercicio_8.rb
+++ Lista_de_Exercicios/Exercicio_8.rb
@@ -1,16 +1,9 @@
-def estado
-    x = 1
-    puts "Digite o nome de 5 estados"
-   
-    while x <= 5
-        num_estado = gets.chomp.to_s
-        num_estado.new = []
-        x += 1
-    end
-    puts "Sua lista tem um total de #{num_estado.count()} elemento"
+names = ['Joãozinho', 'Manoel', 'Juca']
+
+name = 'Anderson Freitas'
+
+names.each do |name|
+    puts name
 end
 
-
-
-
-puts "#{estado}"
+puts name
