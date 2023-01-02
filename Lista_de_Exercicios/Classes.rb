88cec396e96492bc96de1727ebce93f7e5ce6f11
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-02 18:46:54 -0300

    Chamando dois metodos de uma mesma classe

--- Lista_de_Exercicios/Classes.rb
+++ Lista_de_Exercicios/Classes.rb
@@ -2,11 +2,17 @@ class Cadastro
     def nome
         puts "Digite seu nome: "
         name = gets.chomp.to_s
+    end
+
+    def idade
         puts "Digite sua idade: "
         idade = gets.chomp.to_i
-        puts "Seu nome é #{name} e você tem #{idade}"
+        
     end
 end
 
 # .new chama o método dentro da classe
-Cadastro.new.nome
+# Aqui temos uma forma de chamar o metodo dentro de um puts
+
+puts "Seu nome é #{Cadastro.new.nome} e você tem #{Cadastro.new.idade} anos"
+
