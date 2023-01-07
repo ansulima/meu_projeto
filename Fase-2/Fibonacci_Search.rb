b7503ef3fe1397176bbb65c4fbce2cc746154da9
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-07 19:14:16 -0300

    usando as metricas para o calculo

--- Fase-2/Fibonacci_Search.rb
+++ Fase-2/Fibonacci_Search.rb
@@ -1,4 +1,11 @@
-a = "Oi meu nome é Anderson"
-nome = a.split("_")
+def fibonacci_search(array, element)
+    lim_inferior = 0
+    lim_superior = array.length - 1
+    k = 0
+    f = [0,1]
+    while r > f[k]
+        k += 1
+        f[k] = f[k - 1] + f[k - 2]
+    end
 
-puts nome
+    
