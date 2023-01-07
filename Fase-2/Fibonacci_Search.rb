93e30385379e9f4a9855c30ac6318ff5ddb93181
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-07 19:40:38 -0300

    erro corrigido o sinal na linha 16 estava errado

--- Fase-2/Fibonacci_Search.rb
+++ Fase-2/Fibonacci_Search.rb
@@ -1,16 +1,19 @@
+array = [2, 3, 4, 10, 40]
+element = 2
+
 def fibonacci_search(array, element)
     l = 0
     r = array.length - 1
     k = 0
     f = [0,1]
-    while r > f[k]
+    while r > f[k] - 1
         k += 1
         f[k] = f[k - 1] + f[k - 2]
     end
 
     while l <= r
         i = [l, r - f[k - 1] + 1].max # calcula o indice do elemento no array que será comparado com o elemento que estamos procurando
-        if element > array[i]
+        if element < array[i]
             r = i - 1
             k -= 2
         
@@ -24,8 +27,7 @@ def fibonacci_search(array, element)
     return -1
 end
 
-array = [2, 3, 4, 10, 40]
-element = 10
+
 index = fibonacci_search(array, element)
 
 puts "A posição do elemento #{element} no array é #{index}"
