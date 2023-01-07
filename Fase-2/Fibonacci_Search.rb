40b11dfe4893ec556fb017a1f1740be1019983ef
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-07 19:34:45 -0300

    Retornando - 1

--- Fase-2/Fibonacci_Search.rb
+++ Fase-2/Fibonacci_Search.rb
@@ -1,6 +1,6 @@
 def fibonacci_search(array, element)
-    lim_inferior = 0
-    lim_superior = array.length - 1
+    l = 0
+    r = array.length - 1
     k = 0
     f = [0,1]
     while r > f[k]
@@ -8,4 +8,24 @@ def fibonacci_search(array, element)
         f[k] = f[k - 1] + f[k - 2]
     end
 
-    
+    while l <= r
+        i = [l, r - f[k - 1] + 1].max # calcula o indice do elemento no array que será comparado com o elemento que estamos procurando
+        if element > array[i]
+            r = i - 1
+            k -= 2
+        
+        elsif element > array[i]
+            l = i + 1
+            k -= 2
+        else
+            return i
+        end
+    end
+    return -1
+end
+
+array = [2, 3, 4, 10, 40]
+element = 10
+index = fibonacci_search(array, element)
+
+puts "A posição do elemento #{element} no array é #{index}"
