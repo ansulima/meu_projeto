7adc149fb7b855c574f82cd1f8ec9b95aab63267
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-09 19:41:50 -0300

    aplicando o select sort em um array

--- Fase-2/select_sort.rb
+++ Fase-2/select_sort.rb
@@ -1,13 +1,16 @@
 # select_sort
-=begin 
+
 def selection_sort(array)
     n = array.length
     n.times do |i|
+        min_index = i
+        (i + 1).upto(n - 1) do |j|
+            min_index = j if array [j] < array[min_index]
+        end
+        array[i], array[min_index] = array[min_index], array[i] if min_index != i
+    end
+    array
 end
-=end
 
-array = [1, 2]
-n = array.length
-n.times do |i|
-    puts "iteração #{i}: #{array[i]}"
-end
+# testando o metodo
+p selection_sort([8, 5, 2, 9, 7, 6, 3, 1, 4])
