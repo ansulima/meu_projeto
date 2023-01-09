6f41e6e586126a2c03677c7cb8c7023883dfa12f
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-09 19:36:21 -0300

    entendo o n.times

---
+++ Fase-2/select_sort.rb
@@ -0,0 +1,13 @@
+# select_sort
+=begin 
+def selection_sort(array)
+    n = array.length
+    n.times do |i|
+end
+=end
+
+array = [1, 2]
+n = array.length
+n.times do |i|
+    puts "iteração #{i}: #{array[i]}"
+end
