7fec7daaa11982203ce4d6bf50ade8912f0eaa1f
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-10 22:02:01 -0300

    finalizando a logica

--- Fase-2/merge_sort.rb
+++ Fase-2/merge_sort.rb
@@ -8,3 +8,31 @@ def merge_sort(arr)
 
     merge(left, right)
 end
+
+def merge(left, right)
+    result = []
+    i = 0
+    j = 0
+
+    while i < left.length && j < right.length
+        if left[i] <= right[i]
+            result << left[i]
+            i += 1
+        else
+            result << right[i]
+            j += 1
+        end
+    end
+
+    while i < left.length
+        result << left[i]
+        i += 1
+    end
+
+    while j < right.length
+        result << right[j]
+        j += 1
+    end
+
+    result
+end
