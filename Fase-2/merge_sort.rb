b8fe91d9090eed3b071781361f33c561ccacb826
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-12 20:34:31 -0300

    MEtodo left undefined

--- Fase-2/merge_sort.rb
+++ Fase-2/merge_sort.rb
@@ -5,23 +5,40 @@ def merge_sort(array)
     end
 
     middle = array.length / 2
-    left = merge_sort(array[0...minddle])
-    right = merge_sort(array[minddle..-1])
+    left = merge_sort(array[0...middle])
+    right = merge_sort(array[middle..-1])
 
-    merge_sort(left, right)
+    merge(left, right)
 
 end
 
-def merg(left, right)
+def merge(left, right)
     result = []
     i = 0
     j = 0
 
     while i < left.length && j < right.length
-        if left[i] <== right[j]
+        if left[i] <= right[j]
             result << left{i}
             i += 1
+        else
+            result << right[j]
+            j += 1
+        end
+    end
+
+    while i < left.length
+        result << left[i]
+        i += 1
+    end
+
+    while j < right.length
+        result << right[j]
+        j += 1
+    end
+
+    result
 end
 array = [12, 11, 13, 5, 6, 7]
-
-puts "o Array #{array} #{merge_sort(array)}"
+sorted_array = merge_sort(array)
+puts sorted_array
