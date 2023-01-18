acfa152df575a52246d3511218037c4528df01de
Anderson Freitas <freitaslimaanderson@gmail.com>
2023-01-17 23:35:33 -0300

    corrigindo erro de lógica

--- Fase-2/Minimum_sum.rb
+++ Fase-2/Minimum_sum.rb
@@ -1,10 +1,10 @@
-a = [1, 2, 3]
-b = [4, 5, 6]
+a = [1, 2, -3]
+b = [-2, 3, -5]
 
-c = a.flat_map{ |x| b.map{|y| x * y}}
+c = a.flat_map { |x| b.map{ |y| x * y}}
 
 min_val = c.min
 
 result = c.sum
 
-puts result
+puts result 
